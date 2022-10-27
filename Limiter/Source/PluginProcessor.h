766f7d2c456e56e668edd4476aa48d27948207af
Matthijs Hollemans <mail@hollance.com>
2022-10-27 11:56:02 +0200

    update to JUCE 7

--- Limiter/Source/PluginProcessor.h
+++ Limiter/Source/PluginProcessor.h
@@ -2,81 +2,73 @@
 
 #include <JuceHeader.h>
 
-class MDALimiterAudioProcessor : public juce::AudioProcessor,
-                                 private juce::ValueTree::Listener
+class MDALimiterAudioProcessor : public juce::AudioProcessor
 {
 public:
-  MDALimiterAudioProcessor();
-  ~MDALimiterAudioProcessor() override;
+    MDALimiterAudioProcessor();
+    ~MDALimiterAudioProcessor() override;
 
-  void prepareToPlay(double sampleRate, int samplesPerBlock) override;
-  void releaseResources() override;
-  void reset() override;
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void reset() override;
 
-  bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
+    bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
 
-  void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
+    void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
 
-  juce::AudioProcessorEditor *createEditor() override;
-  bool hasEditor() const override { return true; }
+    juce::AudioProcessorEditor *createEditor() override;
+    bool hasEditor() const override { return true; }
 
-  const juce::String getName() const override;
+    const juce::String getName() const override;
 
-  bool acceptsMidi() const override { return false; }
-  bool producesMidi() const override { return false; }
-  bool isMidiEffect() const override { return false; }
-  double getTailLengthSeconds() const override { return 0.0; }
+    bool acceptsMidi() const override { return false; }
+    bool producesMidi() const override { return false; }
+    bool isMidiEffect() const override { return false; }
+    double getTailLengthSeconds() const override { return 0.0; }
 
-  int getNumPrograms() override;
-  int getCurrentProgram() override;
-  void setCurrentProgram(int index) override;
-  const juce::String getProgramName(int index) override;
-  void changeProgramName(int index, const juce::String &newName) override;
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram(int index) override;
+    const juce::String getProgramName(int index) override;
+    void changeProgramName(int index, const juce::String &newName) override;
 
-  void getStateInformation(juce::MemoryBlock &destData) override;
-  void setStateInformation(const void *data, int sizeInBytes) override;
+    void getStateInformation(juce::MemoryBlock &destData) override;
+    void setStateInformation(const void *data, int sizeInBytes) override;
 
-  juce::AudioProcessorValueTreeState apvts { *this, nullptr, "Parameters", createParameterLayout() };
+    juce::AudioProcessorValueTreeState apvts { *this, nullptr, "Parameters", createParameterLayout() };
 
 private:
-  juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 
-  void valueTreePropertyChanged(juce::ValueTree &, const juce::Identifier &) override
-  {
-    _parametersChanged.store(true);
-  }
+    void update();
+    void resetState();
 
-  std::atomic<bool> _parametersChanged { false };
+    // The maximum amplitude you want the sound to have. Louder sounds will be
+    // reduced to approximately this level. With a very slow attack it may take
+    // a long time before the sound drops below the threshold level, if ever.
+    // In soft knee mode, the threshold only indirectly represents the maximum
+    // amplitude, but it still serves the same purpose.
+    float _threshold;
 
-  void update();
-  void resetState();
+    // The attack time determines how quickly the limiter responds when the sound
+    // level approaches (in soft knee mode) or exceeds the threshold (hard knee).
+    float _attack;
 
-  // The maximum amplitude you want the sound to have. Louder sounds will be
-  // reduced to approximately this level. With a very slow attack it may take
-  // a long time before the sound drops below the threshold level, if ever.
-  // In soft knee mode, the threshold only indirectly represents the maximum
-  // amplitude, but it still serves the same purpose.
-  float _threshold;
+    // The release time determines how quickly the gain level resets itself after
+    // the limiter no longer needs to be active.
+    float _release;
 
-  // The attack time determines how quickly the limiter responds when the sound
-  // level approaches (in soft knee mode) or exceeds the threshold (hard knee).
-  float _attack;
+    // Use this for boosting the output level when setting a low threshold.
+    float _trim;
 
-  // The release time determines how quickly the gain level resets itself after
-  // the limiter no longer needs to be active.
-  float _release;
+    // Choose between hard knee and soft knee modes.
+    bool _softKnee;
 
-  // Use this for boosting the output level when setting a low threshold.
-  float _trim;
+    // The current gain level. When the audio signal exceeds or approaches the
+    // threshold, the gain level is lowered to reduce the amplitude. The limiter
+    // essentially calculates a gain signal over time that is used to prevent the
+    // audio from becoming too loud. This is the gain signal's most recent value.
+    float _gain;
 
-  // Choose between hard knee and soft knee modes.
-  bool _softKnee;
-
-  // The current gain level. When the audio signal exceeds or approaches the
-  // threshold, the gain level is lowered to reduce the amplitude. The limiter
-  // essentially calculates a gain signal over time that is used to prevent the
-  // audio from becoming too loud. This is the gain signal's most recent value.
-  float _gain;
-
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MDALimiterAudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MDALimiterAudioProcessor)
 };
